dcba7556dc4840726662857f5effe8032bae1dc5
NexusI9 <nassim.elkhantour@gmail.com>
2022-11-09 20:38:02 -0500

    Added onclick translation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import './sheet.scss';
 import { Homepage, Reader, Settings } from './components';
 import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 
 function App() {
 
   const [url, setUrl] = useState();
   const [theme, setTheme] = useState('white');
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect( () => {
 
     document.querySelector('body').setAttribute('data-theme', theme);
+    if(searchParams.get('article')){
+      setUrl(searchParams.get('article'));
+    }
 
-  }, [theme]);
+    if(url){ setSearchParams({article:url}); }
+
+  }, [theme,url]);
 
   return (
-    <div id="wrapper">
-      {!url && <Homepage onSubmit={ (e) => setUrl(e) } /> }
-      {url && <Reader url={url} /> }
-      {url && <Settings onChangeTheme={ (e) => setTheme(e) } /> }
-    </div>
+      <div id="wrapper">
+        {!url && <Homepage onSubmit={ (e) => setUrl(e) } /> }
+        {url && <Reader url={url} /> }
+        {url && <Settings onChangeTheme={ (e) => setTheme(e) } onPrevParagraph = { () => 0 } onNextParagraph= { () => 0}/> }
+      </div>
   );
 }
 
---
+++ src/lib/zhongwendico.js
@@ -0,0 +1,111 @@
+
+export default class ZhongwenDictionary {
+
+    constructor(wordDict, wordIndex, grammarKeywords) {
+        this.wordDict = wordDict;
+        this.wordIndex = wordIndex;
+        this.grammarKeywords = grammarKeywords;
+        this.cache = {};
+    }
+
+
+    static find(needle, haystack) {
+
+        let beg = 0;
+        let end = haystack.length - 1;
+
+        while (beg < end) {
+
+            let mi = Math.floor((beg + end) / 2);
+            let i = haystack.lastIndexOf('\n', mi) + 1;
+
+            let mis = haystack.substr(i, needle.length);
+            if (needle < mis) {
+                end = i - 1;
+            } else if (needle > mis) {
+                beg = haystack.indexOf('\n', mi + 1) + 1;
+            } else {
+                return haystack.substring(i, haystack.indexOf('\n', mi + 1));
+            }
+        }
+
+
+        return null;
+    }
+
+    hasKeyword(keyword) {
+        return this.grammarKeywords[keyword];
+    }
+
+    wordSearch(word, max) {
+
+        let entry = { data: [] };
+
+        let dict = this.wordDict;
+        let index = this.wordIndex;
+
+        let maxTrim = max || 7;
+
+        let count = 0;
+        let maxLen = 0;
+
+        WHILE:
+            while (word.length > 0) {
+
+                let ix = this.cache[word];
+                if (!ix) {
+                    ix = ZhongwenDictionary.find(word + ',', index);
+                    if (!ix) {
+                        this.cache[word] = [];
+                        continue;
+                    }
+                    ix = ix.split(',');
+                    this.cache[word] = ix;
+                }
+
+                for (let j = 1; j < ix.length; ++j) {
+                    let offset = ix[j];
+
+                    let dentry = dict.substring(offset, dict.indexOf('\n', offset));
+
+                    if (count >= maxTrim) {
+                        entry.more = 1;
+                        break WHILE;
+                    }
+
+                    ++count;
+                    if (maxLen === 0) {
+                        maxLen = word.length;
+                    }
+
+                    entry.data.push([dentry, word]);
+                }
+
+                word = word.substr(0, word.length - 1);
+            }
+
+        if (entry.data.length === 0) {
+            return null;
+        }
+
+        entry.matchLen = maxLen;
+        return entry;
+    }
+
+    search(text) {
+
+        let entry = this.wordSearch(text);
+
+        if (entry) {
+            for (let i = 0; i < entry.data.length; i++) {
+                let word = entry.data[i][1];
+                if (this.hasKeyword(word) && (entry.matchLen === word.length)) {
+                    // the final index should be the last one with the maximum length
+                    entry.grammar = { keyword: word, index: i };
+                }
+            }
+        }
+
+        return entry;
+    }
+}
